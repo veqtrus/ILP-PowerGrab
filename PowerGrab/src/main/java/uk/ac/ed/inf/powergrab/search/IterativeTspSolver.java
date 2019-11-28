73b5061f6b7bf6cae3d5102e5bf2629553ac8660
Paul Georgiou <pavlos1998@gmail.com>
2019-11-28 05:45:27 +0000

    Update IterativeTspSolver.java

--- PowerGrab/src/main/java/uk/ac/ed/inf/powergrab/search/IterativeTspSolver.java
+++ PowerGrab/src/main/java/uk/ac/ed/inf/powergrab/search/IterativeTspSolver.java
@@ -39,9 +39,9 @@ public class IterativeTspSolver<N extends TspSolver.Node<N>> implements TspSolve
     }
 
     public List<N> solveNearestNeighbours(Collection<? extends N> nodes) {
-        List<N> result = new ArrayList<N>(nodes.size());
+        ArrayList<N> result = new ArrayList<N>(nodes.size());
         if (nodes.isEmpty()) return result;
-        List<N> available = new LinkedList<N>(nodes);
+        ArrayList<N> available = new ArrayList<N>(nodes);
         N previous;
         if (initialNode == null) {
             previous = available.remove(0);
@@ -67,12 +67,12 @@ public class IterativeTspSolver<N extends TspSolver.Node<N>> implements TspSolve
     }
 
     public List<N> applyHeuristics(Collection<? extends N> nodes) {
-        List<N> result = new ArrayList<N>(nodes);
-        for (int iter = 0; iter < maxIterations; iter++) {
-            List<N> swap = find3Opt(result);
-            if (swap == null) break;
-            result = swap;
-        }
+        ArrayList<N> result = new ArrayList<N>(nodes);
+        if (result.size() < 2) return result;
+        int iter = 0;
+        for (; iter < maxIterations; iter++)
+            if (!threeOpt(result))
+                break;
         return result;
     }
 
@@ -88,30 +88,66 @@ public class IterativeTspSolver<N extends TspSolver.Node<N>> implements TspSolve
         return result;
     }
 
-    private List<N> find3Opt(List<? extends N> nodes) {
-        double minLength = totalDistance(nodes);
-        List<N> result = null;
+    private boolean threeOpt(ArrayList<N> nodes) {
+        ArrayList<N> keyNodes = new ArrayList<N>(6);
+        int bestI = 0, bestJ = 0, bestK = 0, bestRevAB = 0, bestRevA = 0, bestRevB = 0;
+        double bestDelta = 0.0;
         for (int i = 0, sz = nodes.size(); i < sz; i++) {
             for (int j = i + 1; j < sz; j++) {
                 for (int k = j + 1; k <= sz; k++) {
-                    for (int swapAB = 0; swapAB < 2; swapAB++) {
-                        for (int swapA = 0; swapA < 2; swapA++) {
-                            for (int swapB = 0; swapB < 2; swapB++) {
-                                if (swapAB + swapA + swapB == 0) continue;
-                                List<N> swap = new ArrayList<N>(nodes);
-                                if (swapAB != 0) Collections.reverse(swap.subList(i, k));
-                                if (swapA != 0) Collections.reverse(swap.subList(i, j));
-                                if (swapB != 0) Collections.reverse(swap.subList(j, k));
-                                double length = totalDistance(swap);
-                                if (length < minLength) {
-                                    result = swap;
-                                    minLength = length;
+                    for (int revAB = 0; revAB < 2; revAB++) {
+                        for (int revA = 0; revA < 2; revA++) {
+                            for (int revB = 0; revB < 2; revB++) {
+                                if (revAB + revA + revB == 0) continue;
+                                addKeyNodes(keyNodes, nodes, i, j, k);
+                                double before = keyNodesDistance(keyNodes);
+                                rearrange(keyNodes, 1, 3, 5, revAB, revA, revB);
+                                double after = keyNodesDistance(keyNodes);
+                                double delta = after - before;
+                                if (delta < bestDelta) {
+                                    bestDelta = delta;
+                                    bestI = i;
+                                    bestJ = j;
+                                    bestK = k;
+                                    bestRevAB = revAB;
+                                    bestRevA = revA;
+                                    bestRevB = revB;
                                 }
                             }
                         }
                     }
                 }
             }
+        }
+        if (bestDelta < 0.0) {
+            rearrange(nodes, bestI, bestJ, bestK, bestRevAB, bestRevA, bestRevB);
+            return true;
+        }
+        return false;
+    }
+
+    private void addKeyNodes(ArrayList<N> keyNodes, ArrayList<N> nodes, int i, int j, int k) {
+        keyNodes.clear();
+        keyNodes.add(i == 0 ? initialNode : nodes.get(i - 1));
+        keyNodes.add(nodes.get(i));
+        keyNodes.add(nodes.get(j - 1));
+        keyNodes.add(nodes.get(j));
+        keyNodes.add(nodes.get(k - 1));
+        keyNodes.add(k == nodes.size() ? null : nodes.get(k));
+    }
+
+    private void rearrange(ArrayList<N> nodes, int i, int j, int k, int revAB, int revA, int revB) {
+        if (revAB != 0) Collections.reverse(nodes.subList(i, k));
+        if (revA != 0) Collections.reverse(nodes.subList(i, j));
+        if (revB != 0) Collections.reverse(nodes.subList(j, k));
+    }
+
+    private double keyNodesDistance(ArrayList<N> keyNodes) {
+        double result = 0.0;
+        for (int i = 0; i < 6; i += 2) {
+            N a = keyNodes.get(i), b = keyNodes.get(i + 1);
+            if (a != null && b != null)
+                result += a.distance(b);
         }
         return result;
     }
